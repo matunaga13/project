6989a9cab904b9923a03f9cccb7e60778533ddd4
matunaga13 <teihen13@infoseek.jp>
2020-06-28 09:53:35 +0900

    Updates

---
+++ precache-manifest.28c7bb48e19d979b12fc1a1ff4df6d68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20f10e9fc49e5dcf51bc21b63fb5a02",
+    "url": "/matunaga13/project/index.html"
+  },
+  {
+    "revision": "559a53f21c956b4bd5ba",
+    "url": "/matunaga13/project/static/js/2.d1929aa3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/matunaga13/project/static/js/2.d1929aa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2918ed5075ac0d6638af",
+    "url": "/matunaga13/project/static/js/main.4240e73d.chunk.js"
+  },
+  {
+    "revision": "475b2ec96efd90f1e1ea",
+    "url": "/matunaga13/project/static/js/runtime-main.34327a66.js"
+  }
+]);
